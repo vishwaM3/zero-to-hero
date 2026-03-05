a2a5e12309b690ea834fb5440e025c3516ef13ee
Vishwa M <naikvishwa106@gmail.com>
2026-03-05 18:17:48 +0530

    calculate the natural logarithm of any number.

---
+++ 57-100 python programs/90-100/Write a Python Program to calculate the natural logarithm of any number.py
@@ -0,0 +1,8 @@
+import math
+num = float(input("Enter a number: "))
+if num <= 0:
+   print("Please enter a positive number.")
+else:
+# Calculate the natural logarithm (base e) of the number
+   result = math.log(num)
+   print(f"The natural logarithm of {num} is: {result}")
