de1707470b062de0d7472eab90bbf5ea401f4b41
Vishwa M <naikvishwa106@gmail.com>
2026-03-08 20:54:09 +0530

    Sort Words in Alphabetic Order.

---
+++ 101-120 python programs/Write a Python Program to Sort Words in Alphabetic Order.py
@@ -0,0 +1,9 @@
+my_str = input("Enter a string: ")
+# breakdown the string into a list of words
+words = [word.capitalize() for word in my_str.split()]
+# sort the list
+words.sort()
+# display the sorted words
+print("The sorted words are:")
+for word in words:
+   print(word)
