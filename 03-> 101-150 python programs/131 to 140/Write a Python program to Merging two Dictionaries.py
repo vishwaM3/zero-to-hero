411c2bdb3e35348485ac84bc14431ea1fab7531d
Vishwa M <naikvishwa106@gmail.com>
2026-03-29 20:17:40 +0530

    program to Merging two Dictionaries.

---
+++ 03-> 101-150 python programs/131 to 140/Write a Python program to Merging two Dictionaries.py
@@ -0,0 +1,6 @@
+# 1. Using the update() method:
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'c': 3, 'd': 4}
+dict1.update(dict2)
+# The merged dictionary is now in dict1
+print("Merged Dictionary (using update()):", dict1)
