c9c8f9517e9ab24dd9488c137b454fb3f29b721e
Vishwa M <naikvishwa106@gmail.com>
2026-04-04 20:26:42 +0530

    slove  new problem

---
+++ 03-> 101-150 python programs/131 to 140/Write a Python program to sort Python Dictionaries by Key or Value.py
@@ -0,0 +1,5 @@
+sample_dict = {'apple': 3, 'banana': 1, 'cherry': 2, 'date': 4}
+sorted_dict_by_keys = dict(sorted(sample_dict.items()))
+print("Sorted by keys:")
+for key, value in sorted_dict_by_keys.items():
+   print(f"{key}: {value}")
